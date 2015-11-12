d9ee566d4cdee79335cf8dedba21b525fc73c2c1
Steven <c00135843@itcarlow.ie>
2015-11-12 11:28:57 +0000

    drawing enemy

--- Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
+++ Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
@@ -4,10 +4,11 @@
 
 #include "stdafx.h"
 #include <SDL.h>
+#include "Enemy.h"
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
 
 const int MENU = 0, PLAY = 1;
 int gameState;
@@ -22,32 +23,14 @@ int wmain()
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
 	//SDL
+	Renderer r = Renderer(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+	SDL_Point ePos;
+	ePos.x = 100;
+	ePos.y = 100;
+	Enemy en = Enemy(ePos, 77, 62, r, 0);
 #pragma region SDL STUFF
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else {
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL) {
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else //////
-		{
-			//Create Renderer for the Window
-			gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-			if (gRenderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				return 0;
-			}
-			else {
-				//Initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				screenSurface = SDL_GetWindowSurface(window);
-			}
+
 			bool quit = false;
 
 			SDL_Event e;
@@ -57,25 +40,21 @@ int wmain()
 				{
 				}
 				//controls gameState added in game menu feature
+				r.Begin();
 				switch (gameState) 
 				{
+					
+					
 				case MENU:
+					en.Draw(r);
 					break;
 				case PLAY:
 					break;
+					
 				}
+				r.End();
 			}
-		}
-	}
 
-#pragma endregion
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+	return EXIT_SUCCESS;
 }
 
