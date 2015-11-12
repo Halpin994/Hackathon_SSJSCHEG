989afa7a476acaf486c6f48e94dbef28bf4c12aa
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2015-11-12 12:04:31 +0000

    Menu done

--- Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
+++ Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
@@ -5,6 +5,7 @@
 #include "stdafx.h"
 #include <SDL.h>
 #include "Enemy.h"
+#include "Menu.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 900;
@@ -29,6 +30,23 @@ int wmain()
 	ePos.x = 100;
 	ePos.y = 100;
 	Enemy en = Enemy(ePos, 77, 62, r, 0);
+
+	// MENU
+	SDL_Point bPos;
+	bPos.x = 0;
+	bPos.y = 0;
+	Menu background = Menu(bPos, SCREEN_WIDTH, SCREEN_HEIGHT, r, 0, "background");
+
+	SDL_Point pPos;
+	pPos.x = SCREEN_WIDTH/3;
+	pPos.y = SCREEN_HEIGHT/4;
+	Menu playBtn = Menu(pPos, 263, 44, r, 0, "playBtn");
+
+	SDL_Point e2Pos;
+	e2Pos.x = (SCREEN_WIDTH / 2) - 50;
+	e2Pos.y = (SCREEN_HEIGHT / 2) -20;
+	Menu exitBtn = Menu(e2Pos, 111, 45, r, 0, "exitBtn");
+
 #pragma region SDL STUFF
 
 			bool quit = false;
@@ -46,9 +64,35 @@ int wmain()
 					
 					
 				case MENU:
-					en.Draw(r);
+
+					//detect button click
+					if (e.type == SDL_MOUSEBUTTONDOWN) 
+					{
+						//If the left mouse button was pressed
+						if (e.button.button == SDL_BUTTON_LEFT) 
+						{
+							//Get the mouse offsets
+							int mouse_x = e.button.x;
+							int mouse_y = e.button.y;
+
+							if (playBtn.IsClicked(mouse_x, mouse_y)) 
+							{
+								gameState = PLAY;
+							}
+							else if (exitBtn.IsClicked(mouse_x, mouse_y))
+							{
+								quit = true;
+							}
+						}
+					}// End detect button click
+
+
+					background.Draw(r);
+					playBtn.Draw(r);
+					exitBtn.Draw(r);
 					break;
 				case PLAY:
+					en.Draw(r);
 					break;
 					
 				}
