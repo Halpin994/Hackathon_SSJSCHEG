44c5c9d55a5e0e0833972d677ceac4bbe411797f
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2015-11-12 10:42:37 +0000

    Update loop

--- Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
+++ Hackathon_SSJSCHEG/Hackathon_SSJSCHEG.cpp
@@ -8,42 +8,66 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const int MENU = 0, PLAY = 1;
+int gameState;
+
+SDL_Renderer* gRenderer = NULL;
+
 int wmain()
 {
+	gameState = MENU;
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
-
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
+	//SDL
 
+#pragma region SDL STUFF
 	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
-	else
-	{
+	else {
 		//Create window
 		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
+		if (window == NULL) {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
-		else
+		else //////
 		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			//Create Renderer for the Window
+			gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				return 0;
+			}
+			else {
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				screenSurface = SDL_GetWindowSurface(window);
+			}
+			bool quit = false;
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_Event e;
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
+			while (!quit) {
+				while (SDL_PollEvent(&e) != 0) 
+				{
+				}
+				//controls gameState added in game menu feature
+				switch (gameState) 
+				{
+				case MENU:
+					break;
+				case PLAY:
+					break;
+				}
+			}
 		}
 	}
+
+#pragma endregion
 
 	//Destroy window
 	SDL_DestroyWindow(window);
